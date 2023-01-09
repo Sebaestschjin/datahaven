1f6d028fee42a15ad70f28808c2b33815ee4ab27
Sebastian Stern <sebastian.stern.42@gmail.com>
2023-01-09 17:07:07 +0100

    Add script to parse scenario data

---
+++ src/scenario.ts
@@ -0,0 +1,26 @@
+import { existsSync, mkdirSync, readFileSync, writeFileSync } from "fs";
+
+const main = () => {
+  const scenarioFile = readFileSync("raw/scenarios.json", {
+    encoding: "utf-8",
+  });
+  const scenarios = JSON.parse(scenarioFile) as Record<string, any>;
+
+  for (const [id, scenario] of Object.entries(scenarios)) {
+    const campaign = scenario.campaign || "Other";
+
+    const scenarioPath = `data/scenario/${campaign}`.replace(":", "_");
+    if (!existsSync(scenarioPath)) {
+      mkdirSync(scenarioPath);
+    }
+
+    const scenarioContent = JSON.stringify(scenario, null, 2);
+    const name = scenario.name.replace(/:\?/, "_");
+    const filePath = `${id} - ${name}`;
+    writeFileSync(`${scenarioPath}/${filePath}.json`, scenarioContent, {
+      encoding: "utf-8",
+    });
+  }
+};
+
+main();
